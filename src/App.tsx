24fb06ebc1997549e319acea415a09612f0b293e
sakshamsd <saksham@pegotec.net>
2024-09-22 19:40:42 +0545

    basic components setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,56 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "./components/Card";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/Select";
+import { Button } from "./components/Button";
+import { Label } from "./components/Label";
+import { Input } from "./components/Input";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img
-                    src={logo}
-                    className="App-logo"
-                    alt="logo"
-                />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Name</Label>
+                                <Input
+                                    id="name"
+                                    placeholder="Name of your project"
+                                />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="framework">Framework</Label>
+                                <Select>
+                                    <SelectTrigger id="framework">
+                                        <SelectValue placeholder="Select" />
+                                    </SelectTrigger>
+                                    <SelectContent position="popper">
+                                        <SelectItem value="next">Next.js</SelectItem>
+                                        <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                        <SelectItem value="astro">Astro</SelectItem>
+                                        <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Button variant="outline">Cancel</Button>
+                    <Button>Deploy</Button>
+                </CardFooter>
+            </Card>
         </div>
     );
 }
